8cf177f5404d31562df5cb39d2355a31f47435d6
Artem Kudryavtsev <himynameisartem@icloud.com>
2026-02-19 13:46:48 +0300

    initial commit

---
+++ RenovationCalculator/RenovationCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovationCalculatorApp.swift
+//  RenovationCalculator
+//
+//  Created by Artem Kudryavtsev on 19.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct RenovationCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
